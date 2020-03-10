67e9f4b8cbc682fd9d191e06e40b5dbcffb0fcd4
MladenBogomirov <m.bogomirov@gmail.com>
2020-03-10 21:14:58 +0200

    pics Make the search input a CONTROLLED components
    * Start working on the form submit handler

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,19 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    if (event.target.value.length % 3 === 0) {
-      console.log(event.target.value);
-    }
-  }
+  state = {
+    term: ''
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    console.log(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form action="" className="ui form">
+        <form action="" className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label htmlFor="">Image search</label>
-            <input type="text" onChange={this.onInputChange}/>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
