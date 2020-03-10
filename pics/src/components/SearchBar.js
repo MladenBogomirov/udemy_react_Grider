7db4c4f6146e1874494127a28489fdef6cd59121
MladenBogomirov <m.bogomirov@gmail.com>
2020-03-10 19:44:19 +0200

    pics Set up the SearchBar component.
    *Apply semantic-ui classes

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    if (event.target.value.length % 3 === 0) {
+      console.log(event.target.value);
+    }
+  }
+
   render() {
     return (
-      <div>
-        <form action="">
-          <input type="text"/>
+      <div className="ui segment">
+        <form action="" className="ui form">
+          <div className="field">
+            <label htmlFor="">Image search</label>
+            <input type="text" onChange={this.onInputChange}/>
+          </div>
         </form>
       </div>
     );
